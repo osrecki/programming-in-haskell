c5db2a6a26a84024e7e14e37ae8401f5223c3cc9
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-10-23 00:12:23 +0200

    Add exercises descriptions

--- exercises/lecture-01.hs
+++ exercises/lecture-01.hs
@@ -5,11 +5,17 @@ Maintainer  : dinko.osrecki@gmail.com
 -}
 module Lecture1Exercises where
 
+-- | 1.1
+--   Define function that concatenates three strings, but drops the middle
+--   one if it's shorter than 2 characters (use 'length' function).
 concat3 :: String -> String -> String -> String
 concat3 a b c
   | length b < 2 = a ++ c
   | otherwise    = a ++ b ++ c
 
+-- | 1.2.1
+--   Give a simpler definition of 'showSalary', using only one if-then-else
+--   construct.
 showSalary :: (Num a, Eq a, Show a) => a -> a -> String
 showSalary amount bonus = salaryStr ++ bonusStr
   where
@@ -19,6 +25,9 @@ showSalary amount bonus = salaryStr ++ bonusStr
       then ", and bonus is " ++ show bonus ++ "."
       else ""
 
+-- | 1.2.2
+--   Additionally check that salary is non-negative. If it's negative,
+--   return an adequate message.
 showSalary' :: (Num a, Ord a, Show a) => a -> a -> String
 showSalary' amount bonus
   | amount < 0 = error "Salary must be positive."
