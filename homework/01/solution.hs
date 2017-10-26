3ae91de70e01b47d78f728b8a00ae1ae5616801c
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-10-27 00:13:09 +0200

    Add solution to task 3 of homework 1

--- homework/01/solution.hs
+++ homework/01/solution.hs
@@ -43,3 +43,55 @@ maclaurin = map (\n -> 1 / factorial n) [0..]
 --   which approximates exp from the first 170 terms of the series.
 exp' :: Double -> Double
 exp' n = evaluate n $ take 170 maclaurin
+
+-- | 3. Implement some polymorphic utility functions for working on
+--   lists of key value pairs where keys are strings.
+-- | 3. a)
+--   Implement a function for getting a pair with a certain key which
+--   should return a list with a single element as a result (or no
+--   elements if key doesn’t exist).
+findItem :: [(String, a)] -> String -> [(String, a)]
+findItem [] _ = []
+findItem (x:xs) k
+  | k == fst x = [x]
+  | otherwise  = findItem xs k
+
+-- | 3. b)
+--   Implement a function that checks if a list contains an element
+--   with a certain key.
+contains :: [(String, a)] -> String -> Bool
+contains xs k = not $ null $ findItem xs k
+
+-- | 3. c)
+--   Implement a function that tries to retrieve a value with a certain\
+--   key or throws an error if the key does not exist.
+lookup' :: [(String, a)] -> String -> a
+lookup' xs k
+  | contains xs k = snd $ head $ findItem xs k
+  | otherwise     = error "key does not exist"
+
+-- | 3. d)
+--   Implement a function that inserts a new key value pair. If the key
+--   already exists then do nothing.
+insert :: [(String, a)] -> (String, a) -> [(String, a)]
+insert xs y@(k, _)
+  | contains xs k = xs
+  | otherwise     = y:xs
+
+-- | 3. e)
+--   Implement a function that removes a key value pair with the
+--   certain key.
+remove :: [(String, a)] -> String -> [(String, a)]
+remove [] _ = []
+remove (x:xs) k
+  | fst x == k = xs
+  | otherwise  = x : remove xs k
+
+-- | 3. f)
+--   Implement a function that updates the value of a certain key
+--   (if the key does not exist, function does nothing).
+update :: [(String, a)] -> String -> a -> [(String, a)]
+update [] _ _ = []
+update (x:xs) k v
+  | fst x == k = (k, v):xs
+  | otherwise  = x : update xs k v
