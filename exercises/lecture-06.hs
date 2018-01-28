76f5219dfd1ab44b11e6266af26cafc2fd300ab0
Dinko Osrecki <dinko.osrecki@gmail.com>
2018-01-28 16:44:44 +0100

    Add solution to 1.1 and 1.2 from lecture 6

---
+++ exercises/lecture-06.hs
@@ -0,0 +1,34 @@
+{-|
+Module      : Lecture6Exercises
+Description : Solutions to in-class exercises for Lecture 6
+Maintainer  : Dinko Osrecki
+-}
+module Lecture6Exercises where
+
+-- | 1.1
+--   Write an accumulator-style recursive definition of length.
+length' :: [a] -> Int
+length' xs = len xs 0
+  where
+    len [] n     = n
+    len (_:ys) n = len ys (n + 1)
+
+-- | 1.2 a
+--   Write an accumulator-style recursive definition of a function
+--   which returns the maximum element at the first position and
+--   the maximum element at the second position in a pair.
+maxUnzip :: [(Int, Int)] -> (Int, Int)
+maxUnzip [] = error "empty list"
+maxUnzip ((x,y):zs) = maxUnzipRec zs (x,y)
+  where
+    maxUnzipRec [] (a,b)         = (a,b)
+    maxUnzipRec ((p,q):rs) (a,b) = maxUnzipRec rs (max p a, max q b)
+
+-- | 1.2 b
+--   Write the same function without an accumulator.
+maxUnzip' :: [(Int, Int)] -> (Int, Int)
+maxUnzip' []      = error "empty list"
+maxUnzip' [(a,b)] = (a,b)
+maxUnzip' ((a,b):xs) = (max a c, max b d)
+  where
+    (c, d) = maxUnzip' xs
