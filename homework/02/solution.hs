7f291e5b72d416f178cc90b2b7a406287d00bd81
Dinko Osrecki <dinko.osrecki@gmail.com>
2018-01-29 18:57:41 +0100

    Add solutions to tasks 1 and 2 from homework 02

---
+++ homework/02/solution.hs
@@ -0,0 +1,59 @@
+{-|
+Module      : Homework2Tasks
+Description : Solutions to homework 2 tasks
+Maintainer  : Dinko Osrecki
+-}
+module Homework2Tasks where
+
+-- | 1
+--   Given a DNA strand, return its RNA complement.
+toRNA :: String -> String
+toRNA = map toRNAChar
+
+toRNAChar :: Char -> Char
+toRNAChar 'G' = 'C'
+toRNAChar 'C' = 'G'
+toRNAChar 'T' = 'A'
+toRNAChar 'A' = 'U'
+toRNAChar _   = error "invalid DNA nucletiode"
+
+-- | 2
+--   Define following functions using recursion.
+-- | 2 a
+--   Define a multiplication function in terms of addition.
+multiply :: Int -> Int -> Int
+multiply a b = sign v
+  where
+    sign = negIf (signum a /= signum b)
+    v = mult (abs a) (abs b)
+
+-- for b >= 0
+mult :: Int -> Int -> Int
+mult _ 0 = 0
+mult a 1 = a
+mult a b = a + mult a (b - 1)
+
+-- | 2 b
+--   Define a division function in terms of subtraction.
+divide :: Int -> Int -> Int
+divide a b = sign v
+  where
+    sign = negIf (signum a /= signum b)
+    v = div' (abs a) (abs b)
+
+-- for a >= 0 && b > 0
+div' :: Int -> Int -> Int
+div' _ 0 = error "division by zero"
+div' a b
+  | a < b     = 0
+  | otherwise = 1 + div' (a - b) b
+
+negIf :: Bool -> Int -> Int
+negIf True x  = - abs x
+negIf False x = abs x
+
+-- | 2 c
+--   Define a function to find the greatest common divisor.
+greatestCD :: Int -> Int -> Int
+greatestCD x 0 = x
+greatestCD x y = greatestCD y (x `mod` y)
