b3f50f659d4896949b58cf4edf3fac370c7c52fb
Dinko Osrecki <dinko.osrecki@gmail.com>
2018-02-07 19:41:55 +0100

    Add solution for task 2 of homework 04

--- homework/04/solution.hs
+++ homework/04/solution.hs
@@ -8,6 +8,7 @@ Maintainer  : Dinko Osrecki
 module Homework4Tasks where
 
 import           Data.Function
+import           Data.List
 
 -- | 1
 --   Define following recursive functions and structures
@@ -49,3 +50,44 @@ zip' :: [a] -> [b] -> [(a,b)]
 zip' = fix (\rec xs ys -> case (xs,ys) of
                             (a:as,b:bs) -> (a,b) : rec as bs
                             _           -> [])
+
+-- | 2 a
+--   Write a function which generates a list of all k-sized
+--   subsets of the given set. Set is represetented with a
+--   list which may contain duplicate elements.
+subsets :: (Eq a) => Int -> [a] -> [[a]]
+subsets n xs = subsetsUniq n $ nub xs
+
+-- for set (list) with unique elements
+subsetsUniq :: (Eq a) => Int -> [a] -> [[a]]
+subsetsUniq 0 _      = [[]]
+subsetsUniq _ []     = []
+subsetsUniq n (x:xs) = ys ++ zs
+  where
+    ys = map (x:) $ subsetsUniq (n-1) xs
+    zs = subsetsUniq n xs
+
+-- | 2 b
+--   Write a function which generates a list of all partitions
+--   of the given set. Each partition is represented as a list
+--   of lists, the sublist being the disjoint subsets of the
+--   given set.
+partitions :: [a] -> [[[a]]]
+partitions [] = error "empty set"
+partitions xs = partitions' xs
+
+partitions' :: [a] -> [[[a]]]
+partitions' []     = [[]]
+partitions' (x:xs) = concatMap (extendPartition x) (partitions' xs)
+
+-- | Extends a given partition into several partitions by
+--   adding the element 'x' in two following ways:
+--    1. add a singleton subset [x]
+--    2. add element x to each existing subset in turn
+--
+--   Example:
+-- >>>  extendPartition 0 [[1],[2,3]]
+-- [[[0,1],[2,3]], [[1],[0,2,3]], [[1],[2,3],[0]]]
+extendPartition :: a -> [[a]] -> [[[a]]]
+extendPartition x []       = [[[x]]]
+extendPartition x (xs:xss) = ((x:xs):xss) : map (xs:) (extendPartition x xss)
