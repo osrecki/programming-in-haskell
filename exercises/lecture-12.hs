b5bf8d2652a1ab964cc1d8850a12597d5073ef15
Dinko Osrecki <dinko.osrecki@gmail.com>
2018-02-20 23:20:26 +0100

    Add solution for task 2 from lecture 12

--- exercises/lecture-12.hs
+++ exercises/lecture-12.hs
@@ -5,6 +5,8 @@ Maintainer  : Dinko Osrecki
 -}
 module Lecture12Exercises where
 
+import           Text.Read (readMaybe)
+
 -- EXERCISE 01 ----------------------------------------------------------------
 
 {-
@@ -29,3 +31,76 @@ threeNumbers = do
 
 readInt :: String -> Int
 readInt = read
+
+-- EXERCISE 02 ----------------------------------------------------------------
+
+{-
+  2.1
+  - Define a function which reads in three strings and outputs them to the
+    screen as one string, while it returns its total length.
+-}
+threeStrings :: IO Int
+threeStrings = do
+  l <- concat <$> sequence [getLine, getLine, getLine]
+  putStrLn l
+  return $ length l
+
+{-
+  2.2
+  - Define a function which reads in a number and returns that number converted
+    into an 'Int'. Input should be repeated until the user enters a number (a
+    string containing only digits).
+-}
+askNumber9 :: IO Int
+askNumber9 = do
+  n <- readMaybeInt <$> getLine
+  maybe askNumber9 return n
+
+readMaybeInt :: String -> Maybe Int
+readMaybeInt = readMaybe
+
+{-
+  2.3 a
+  - Define a function 'askUser m p' which returns an action that prints out 'm',
+    reads in a string from the input, repeats the input until the input string
+    satisfies the function 'p', and then returns the input string.
+-}
+askUser :: String -> (String -> Bool) -> IO String
+askUser m p = do
+  putStrLn m
+  readUntil p
+
+readUntil :: (String -> Bool) -> IO String
+readUntil p = do
+  line <- getLine
+  if p line
+    then return line
+    else readUntil p
+
+{-
+  2.3 b
+  - Generalize the function to:
+    askUser' :: Read a => String -> (String -> Bool) -> IO a
+-}
+askUser' :: Read a => String -> (String -> Bool) -> IO a
+askUser' m p = do
+  putStrLn m
+  readUntil' p
+
+readUntil' :: (Read a) => (String -> Bool) -> IO a
+readUntil' p = do
+  line <- getLine
+  if p line
+    then return $ read line
+    else readUntil' p
+
+{-
+  2.4
+  - Define a function which reads in strings until the user inputs an empty
+    string, and then returns a list of strings received as input.
+-}
+inputStrings :: IO [String]
+inputStrings = takeWhile (not . null) <$> getLines
+
+getLines :: IO [String]
+getLines = lines <$> getContents
